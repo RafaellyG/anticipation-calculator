d742b0856485e95243793c5995e95e553b6559cb
Rafaelly Gruber <rafaelly.gruber@gmail.com>
2021-02-15 17:47:08 -0300

    Theme set up configurations

---
+++ src/theme/tokens.ts
@@ -0,0 +1,46 @@
+const tokens = {
+  borderRadiusSmall: "4px",
+  borderWidthSmall: "1px",
+  boxShadowMedium: "0 2px 7px 0 rgba(0, 0, 0, 0.2)",
+  
+  colorPrimary: "#656565",
+  colorPrimaryLight: "#DDE6E9",
+  colorPrimaryMedium: "#CECECE",
+  colorSecondary: "#5D9CEC",
+  colorSecondaryLight: "#66AFE9",
+  colorSecondaryDark: "#3D75BB",
+  colorSupportError: "#d4716a",
+  colorOpacity: "rgba(0,0,0,0.5)",
+  colorNeutralLight: "#ffffff",
+  colorNeutralMedium: "#999e9c",
+  colorNeutralDark: "#000000",
+
+  colorOpacityPrimary: "rgba(209, 220, 227, 0.18)",
+  colorOpacitySecondary: "rgba(93 , 156, 236, 0.3)",
+  
+  fontFamily: "Source Sans Pro",
+  fontFamilyBold: "Source Sans Pro Bold",
+  fontFamilyItalic: "Source Sans Pro Italic",
+  fontFamilyBoldItalic: "Source Sans Pro BoldItalic",
+
+  fontSizeMedium: "24px",
+  fontSizeSmall: "16px",
+  fontSizexSmall: "14px",
+  fontSizexXSmall: "11px",
+
+  lineHeightGiant: "30px",
+  lineHeightLarge: "20px",
+  lineHeightMedium: "18px",
+  lineHeightSmall: "14px",
+
+  spacingBottomLarge: "32px",
+  spacingBottomMedium: "28px",
+  spacingBottomSmall: "4px",
+
+  spacingLarge: "35px",
+  spacingMedium: "30px",
+  spacingSmall: "20px",
+  spacingxSmall: "10px",
+};
+
+export default tokens;
