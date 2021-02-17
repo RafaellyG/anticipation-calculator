4faae42d663503493d44f22fd3c4ae286ae8ff23
Rafaelly Gruber <rafaelly.gruber@gmail.com>
2021-02-16 22:21:42 -0300

    Add api error handling

---
+++ src/api/errorHandling.ts
@@ -0,0 +1,20 @@
+const errors =
+{
+  500: 'Oh não, parece que nosso serviço está fora do ar no momento.',
+  408: 'Ops! O serviço está demorando muito para responder, tente novamente mais tarde.',
+  400: 'Algo errado não está certo, verifique os valores digitados.',
+  generic: 'Oh não! Um erro inesperado aconteceu, tente novamente mais tarde',
+  internet: 'Parece que sua conexão com a internet está instável nesse momento.',
+};
+
+export const handleResponse = (response: any) => {
+
+  if (response.ok) return response;
+
+  if (response.message === 'Failed to fetch') { // user has internet problems
+    throw new Error(errors.internet);
+  }
+
+  const message = errors[response.status as keyof typeof errors] || errors.generic;
+  throw new Error(message);
+}
