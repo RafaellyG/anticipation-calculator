851a33a51e0805e30bc2b935cae5dea4181775a3
Rafaelly Gruber <rafaelly.gruber@gmail.com>
2021-02-16 22:23:40 -0300

    Add svg loading icon

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,37 @@
+import { Container } from './style';
+
+const Loading = () => {
+  return (
+    <Container>
+      <svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+        viewBox="0 0 100 100" enableBackground="new 0 0 0 0">
+        <circle fill="#5D9CEC" stroke="none" cx="6" cy="10" r="6">
+          <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite"
+            begin="0.1" />
+        </circle>
+        <circle fill="#5D9CEC" stroke="none" cx="26" cy="10" r="6">
+          <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite"
+            begin="0.2" />
+        </circle>
+        <circle fill="#5D9CEC" stroke="none" cx="46" cy="10" r="6">
+          <animate
+            attributeName="opacity"
+            dur="1s"
+            values="0;1;0"
+            repeatCount="indefinite"
+            begin="0.3" />
+        </circle>
+      </svg>
+    </Container>
+  );
+}
+
+export default Loading;
